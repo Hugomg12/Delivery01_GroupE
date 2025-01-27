11cfcf3149b2962fc40f3d4425f55943d1f9efc1
IvanMorenoG <165889593+IvanMorenoG@users.noreply.github.com>
2025-01-27 18:57:15 +0100

    comit

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,32 +1,73 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿
+    using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField]
-    private float Speed = 5.0f;
+    public float velocidadCaminar = 5f;
+    public float fuerzaSalto = 10f;
+    public GameObject GroundCheck;
 
-    Rigidbody2D _rigidbody;
-    private float _horizontalDir; // Horizontal move direction value [-1, 1]
+    private bool enSuelo;
+    private float TamañoGround = 0.4f;
+    public LayerMask capaSuelo;
 
-    void Start()
+    private float coyoteTime = 0.1f;
+    private float coyoteTimeCounter;
+
+    private Rigidbody2D rb;
+    private bool puedeSaltar = true;
+
+    private int saltosRestantes;
+    public int maxSaltos = 2;
+
+    private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        saltosRestantes = maxSaltos;
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        Vector2 velocity = _rigidbody.linearVelocity;
-        velocity.x = _horizontalDir * Speed;
-        _rigidbody.linearVelocity = velocity;
-    }
+        enSuelo = Physics2D.OverlapCircle(GroundCheck.transform.position, TamañoGround, capaSuelo);
 
-    // NOTE: InputSystem: "move" action becomes "OnMove" method
-    void OnMove(InputValue value)
-    {
-        // Read value from control, the type depends on what
-        // type of controls the action is bound to
-        var inputVal = value.Get<Vector2>();
-        _horizontalDir = inputVal.x;
+        float movimiento = Input.GetAxis("Horizontal");
+        bool correr = Input.GetKey(KeyCode.LeftShift);
+
+        rb.linearVelocity = new Vector2(movimiento * velocidadCaminar, rb.linearVelocity.y);
+
+        if (movimiento > 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        else if (movimiento < 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        if (enSuelo)
+        {
+            coyoteTimeCounter = coyoteTime;
+            saltosRestantes = maxSaltos;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && (coyoteTimeCounter > 0 || saltosRestantes > 0))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
+            saltosRestantes--;
+            coyoteTimeCounter = 0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+
+        if (enSuelo)
+        {
+            puedeSaltar = true;
+        }
     }
 }
+
+
+
